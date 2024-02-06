50523ed240fa52ef0f9b2407a4e2a512dc3eb0b0
shubhamskq <79262215+shubhamskq@users.noreply.github.com>
2024-02-06 15:39:05 +0530

    Update Callback function.js

--- Callback function.js
+++ Callback function.js
@@ -1,4 +1,8 @@
 a function passed to another function is known as a call back function.
 For Example:
+Example 1:
 x(function y() {  // Function y is call back function
-  )
+  );
+Example 2:
+setTimeout(function() {
+}, 1000) // -> second argument 
